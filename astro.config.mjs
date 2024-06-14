bf6439aa134d639bd17f84fcafad89fcc1cde1e8
The Wizard of Hahz <info@luvnft.com>
2024-06-14 13:17:23 -0400

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nebulix.unfolding.io",
+  site: "https://healxyz.store",
   integrations: [
     icon(),
     mdx({}),
--- src/components/common/Init.vue
+++ src/components/common/Init.vue
@@ -98,7 +98,7 @@ watch(width, (val) => {
       "%c ♻️🔋+ 🧠👷🏽+ 🗜 = 🚀🍃🌐" +
         "\n%cThis site has a low carbon footprint " +
         "\n%c🪙CREDITS:" +
-        "\n%cTheme based on Nebulix 🌌" +
+        "\n%cTheme based on Healxyz.store 🌌" +
         "\n%cby: https://unfolding.io",
       "font-family:Verdana; font-size: 20px; color: #2A4D47; font-weight:bold; padding: 5px 0; opacity: 0.5; ",
       "font-family:Verdana; font-size: 25px; color: #2A4D47; font-weight:bold; padding: 5px 0; ",
